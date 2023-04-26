c3d4d1ccc77ffbd7fc02b97054613a8a8e18196a
samflo <samflo@kth.se>
2023-04-26 13:16:06 +0200

    Videobakgrund

--- MainGame.py
+++ MainGame.py
@@ -1,38 +1,73 @@
 import sys
 import pygame
+import cv2
+
+#I terminalen: pip install opencv-python
 
 #This is where the mainGame loop and graphics are handled for tetris
 
 pygame.init()
 
 #Set up the screen
-WIN = pygame.display.set_mode((640, 480))
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Tetris")
 
 
+#Set up background video
+cap = cv2.VideoCapture('Background.mp4')
+success, img = cap.read()
+shape = img.shape[1::-1]
+wn = pygame.display.set_mode(shape)
+
+
 clock = pygame.time.Clock()
-fps = 60
+
+
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
+BlACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+def drawWindow(img):
+    WIN.fill(BlACK)
+    
+    wn.blit(pygame.image.frombuffer(img.tobytes(), shape, "BGR"), (0, 0))
+    WIN.blit(pygame.font.SysFont('comicsans', 60).render('TETRIS', 1, RED), (200, 200))
+    pygame.display.update()
+
+fps = 30
 
 #game loop
 def main():
-
+    
     run = True
+    
     while run:
-    #want to handle what happens
+        clock.tick(fps)
+
+        # Read frame from video stream
+        success, img = cap.read()
+
+        # Check if end of video stream
+        if not success:
+            # Restart video stream from the beginning
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            success, img = cap.read()
+
+        # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                pygame.quit()
-                sys.exit()
-                
 
-        WIN.fill((0,0,0))
+        # Draw window
+        drawWindow(img)
 
-        pygame.display.flip()
-
-        clock.tick(fps)
-
-    
 #Om jag förstår rätt så är det här en restraint så att man inte kan köra programmet utanför main funktionen eller MainGame.py filen
 if __name__ == "__main__":
     main()
+    
+pygame.quit()
+sys.exit()
+    
