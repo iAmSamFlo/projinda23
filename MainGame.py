c16bd97c5400f2bde0307b4e1c840e2b9e89f9fa
samflo <samflo@kth.se>
2023-04-26 11:24:04 +0200

    Testar att commita till bransch

--- MainGame.py
+++ MainGame.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 
 #This is where the mainGame loop and graphics are handled for tetris
@@ -5,7 +6,7 @@ import pygame
 pygame.init()
 
 #Set up the screen
-screen = pygame.display.set_mode((640, 480))
+WIN = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("Tetris")
 
 
@@ -13,16 +14,25 @@ clock = pygame.time.Clock()
 fps = 60
 
 #game loop
-while True:
+def main():
+
+    run = True
+    while run:
     #want to handle what happens
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                sys.exit()
+                
 
-    screen.fill((0,0,0))
+        WIN.fill((0,0,0))
 
-    pygame.display.flip()
+        pygame.display.flip()
 
-    clock.tick(fps)
+        clock.tick(fps)
 
+    
+#Om jag förstår rätt så är det här en restraint så att man inte kan köra programmet utanför main funktionen eller MainGame.py filen
+if __name__ == "__main__":
+    main()
